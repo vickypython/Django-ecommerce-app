62acc3f8facaecd73521a99da84a0ed4a219d2c7
vickypython <vickymlucky6@gmail.com>
2025-02-28 13:47:26 +0300

    updated the customers app urls

--- customers/urls.py
+++ customers/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import say_hello
+from . import views
 urlpatterns = [
-    path('hello/',say_hello)
+    path('hello/',views.say_hello,name='say_hello'),
+    path('posts/',views.post_list,name='post_list'),
+    path('<int:post_id>/',views.One_Post,name='post_details'),
+     path('create/',views.Create_post,name='post_create'),
+     path('<int:post_id>/update/',views.Update_post,name='post_update'),
+     path('<int:post_id>/delete/',views.delete_post,name='post_delete')
 ]
