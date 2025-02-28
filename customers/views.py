4db97675b635d932c69f3d4aa11b28ac89a58876
vickypython <vickymlucky6@gmail.com>
2025-02-28 12:56:02 +0300

    updated the views

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404,redirect
 from django.http import HttpResponse
 from .models import Categorie,Products
 # Create your views here.
@@ -10,3 +10,18 @@ def say_hello(request):
         'products':products
     }
     return render(request,'customers/index.html',context)
+def post_list(request):
+    posts=Products.objects.all()
+    return render(request,'customers/index.html',{"posts":posts})
+def One_Post(request,post_id):
+    post=get_list_or_404(Products,id=post_id)
+    return render(request,'customers/index.html',{"posts":post})
+def Create_post(request):
+    if request.method == 'POST':
+        name=request.POST['name']
+        description=request.POST['description']
+        Products.objects.create(name=name,description=description)
+        return redirect('post_list')
+    return render(request,'customers/index.html')
+
+
